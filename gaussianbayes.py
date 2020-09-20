d630f8e49765af25f45b533861b41eb96bff8242
ryan <ryandahunter@gmail.com>
2020-09-19 20:30:54 -0700

    add functioning gaussianbayes

---
+++ gaussianbayes.py
@@ -0,0 +1,10 @@
+import tfidf
+from sklearn.naive_bayes import GaussianNB
+
+def generate_gaussian_naive_bayes_model(train_data, train_labels):
+  
+    vectorized_train_data, tfidf_vect = tfidf.generate_tfidf_matrix(train_data)
+    print("Train data dims: {0}".format(vectorized_train_data.shape))
+    nb_model = GaussianNB()
+    nb_model.fit(vectorized_train_data.toarray(), train_labels)
+    return nb_model, tfidf_vect
