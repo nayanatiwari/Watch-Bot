6cb65d345c46470d2a7c4209df536bfc28ebaf2e
ryan <ryandahunter@gmail.com>
2020-09-19 20:01:23 -0700

    add gaussianbayes to models

--- generate_model.py
+++ generate_model.py
@@ -2,14 +2,14 @@ import os.path
 import data_util
 import copy
 import random
+import predictor
 from naivebayes import generate_naive_bayes_model
+from gaussianbayes import generate_gaussian_naive_bayes_model
 from joblib import dump, load
 
-model_file = "models/naivebayes.model"
-matrix_file = "models/naivebayes.matrix"
+models = ["models/naivebayes.model", "models/gaussiannaivebayes.model"]
+matrices = ["models/naivebayes.matrix", "models/gaussiannaivebayes.matrix"]
 
-S_FNAME = "/data/suicidal_data.txt"
-N_FNAME = "/data/suicidal_data.txt"
 T_T_SPLIT = .5  # proportion of data used for training
 
 def get_data_as_list_of_documents():
@@ -28,7 +28,7 @@ def split_docs_train_test(docs, train_test_split):
     
     return train_docs, docs_copy
 
-def get_split_data(suicidal_fname, not_fname, train_test_split):
+def get_split_data(train_test_split):
     suicidal_docs, not_docs = get_data_as_list_of_documents()
     train_suicidal_docs, test_suicidal_docs =\
                                 split_docs_train_test(suicidal_docs,
@@ -46,17 +46,34 @@ def get_data_labels(tr_s_d, tr_n_d, te_s_d, te_n_d):
 
     return train_labels, test_labels
 
-def generate_and_save_model():
+def generate_and_save_model(model_file, matrix_file):
 
-    tr_s_d, tr_n_d, te_s_d, te_n_d = get_split_data(S_FNAME, N_FNAME, T_T_SPLIT)
+    classifier_to_use = os.path.basename(model_file).split(".")[0]
+    tr_s_d, tr_n_d, te_s_d, te_n_d = get_split_data(T_T_SPLIT)
     train_labels, test_labels = get_data_labels(tr_s_d, tr_n_d, te_s_d, te_n_d)
     train_data = tr_s_d + tr_n_d
     test_data = te_s_d + te_n_d
 
-    nb_model, tfidf_vect  = generate_naive_bayes_model(train_data, train_labels)
+    if classifier_to_use == "naivebayes":
+        nb_model, tfidf_vect  = generate_naive_bayes_model(train_data, train_labels)
+    elif classifier_to_use == "gaussiannaivebayes":
+        nb_model, tfidf_vect  = generate_gaussian_naive_bayes_model(train_data, train_labels)
+    else:
+        print("Invalid model type: {0}".format(classifier_to_use))
+        exit()
     
     dump(nb_model, model_file)
     dump(tfidf_vect, matrix_file)
+    
+    predictions = predictor.predict_group(nb_model, test_data, tfidf_vect)
+    correct = 0
+    for i, label in enumerate(predictions):
+        if label == test_labels[i]:
+            correct += 1
+
+    print("Percentage correct for new model: {0}".format(correct/len(test_labels)))
+
+
 
 def load_model_and_matrix(model_file, matrix_file):
     model = load(model_file)
@@ -64,27 +81,31 @@ def load_model_and_matrix(model_file, matrix_file):
 
     return model, matrix
 
-def predict_based_on_model(model, test_data, tfidf_vect):
-    vectorized_test_data, tfidf_vect = tfidf.generate_tfidf_matrix(test_data, test=True, tfidf_vect=tfidf_vect)
-    print("Test data dims: {0}".format(vectorized_test_data.shape))
-    predicted = model.predict(vectorized_test_data)
-    return predicted
-
-def predict_individual_doc(model, doc, tfidf_vect):
-    vectorized_doc, tfidf_vect = tfidf.generate_tfidf_matrix([doc], test=True, tfidf_vect=tfidf_vect)
-    return model.predict(vectorized_doc)
-
 if __name__ == "__main__":
+
+    print("Models available to generate: {0}".format(models))
+    index = ""
+    while not index.isnumeric():
+        index = input("Select your model by index: ")
+        if not index.isnumeric():
+            print("Invalid index, please enter an integer.")
+        else:
+            i = int(index)
+            if i >= len(models):
+                print("Index outside of range of model list.")
+
+    model_file = models[i]
+    matrix_file = model_file[:-6] + ".matrix"
 
     if os.path.isfile(model_file):
         overwrite = input("A saved model ({0}) exists, overwrite? (y/n)".format(model_file))
         if overwrite == 'y':
-            generate_and_save_model()
+            generate_and_save_model(model_file, matrix_file)
         else:
             load_model_and_matrix(model_file, matrix_file)
             exit()
     else:
-        generate_and_save_model()
+        generate_and_save_model(model_file, matrix_file)
 
 
     
